49469c230b725d8cae2b79e23cd25c4420815abc
BOLK <kinbolb@gmail.com>
2023-10-15 23:28:46 -0500

    ejercicio 1 aún falta controlar errores como división entre 0 y cuando el orden de los números es invertido

---
+++ ejercicio-1.java
@@ -0,0 +1,18 @@
+class one{
+    public static void main (String []args){
+        var num = 10;
+        var num2 = 5;
+        var suma = num + num2;
+        var resta = num - num2;
+        var division = num / num2;
+        var multiplicacion = num * num2;
+        var modulo = num % num2;
+
+        System.out.println("Los numeros son "+ num + " y " + num2);
+        System.out.println("Suma = " + suma);
+        System.out.println("Resta = " + resta);
+        System.out.println("División = "+ division);
+        System.out.println("Multiplicación = " + multiplicacion);
+        System.out.println("Modulo = " + modulo);
+    }
+}
